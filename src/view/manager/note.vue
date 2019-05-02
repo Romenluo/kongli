0306fd130bb75873e50d51ff5091c5e9a465710c
luomen1126 <1689488576@qq.com>
2019-05-02 15:02:24 +0800

    文章管理

--- src/view/manager/note.vue
+++ src/view/manager/note.vue
@@ -2,7 +2,6 @@
   <el-tabs type="border-card">
     <el-tab-pane label="文章信息">
       <manager-note></manager-note>
-
     </el-tab-pane>
     <el-tab-pane label="添加文章">
       <editor-note :category="category"></editor-note>
